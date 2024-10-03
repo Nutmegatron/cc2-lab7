60d00432f5d40f1ec227eefc45d823c7e7a9443f
Nutmegatron <cadg.toralba@gmail.com>
2024-10-03 17:08:14 +0800

    corrected grammar in one of the comments

--- Lab7.java
+++ Lab7.java
@@ -19,7 +19,7 @@ public class Lab7 {
             System.out.print("Enter your password: ");
             String userPassword = myPassword.nextLine();
 
-//            using regular expressions to check with the password has an uppercase letter or a digit, also checking if the length is 8 letters or fewer
+//            using regular expressions to check if the password has an uppercase letter or a digit, also checking if the length is 8 letters or fewer
             if (userPassword.length() <= 8 && Pattern.compile("[A-Z]").matcher(userPassword).find() && Pattern.compile("[0-9]").matcher(userPassword).find()) {
                 System.out.println("Your password is valid!");
                 break;
